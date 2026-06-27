33bc41ba1a0065e68201a156532e515185815495
Marcelo Araya <marceliux@tutamail.com>
2026-06-27 03:41:31 -0600

    chore: fix indentation (tabs vs spaces) across codebase

--- src/controllers/Auth.controller.ts
+++ src/controllers/Auth.controller.ts
@@ -9,249 +9,249 @@ import { getAllUsers } from "../helpers/getAllUsers";
 import { toggleActive } from "../helpers/toggleActive";
 
 interface AuthInput {
-  authInput: {
-    email: User["email"];
-    password: User["password"];
-  };
+	authInput: {
+		email: User["email"];
+		password: User["password"];
+	};
 }
 
 interface UserInput {
-  userInput: {
-    name: User["name"];
-    email: User["email"];
-    lastName: User["lastName"];
-    password: User["password"];
-    avatar: User["avatar"];
-  };
+	userInput: {
+		name: User["name"];
+		email: User["email"];
+		lastName: User["lastName"];
+		password: User["password"];
+		avatar: User["avatar"];
+	};
 }
 interface ForgotPasswordInput {
-  forgotPasswordInput: {
-    email: User["email"];
-    otp: User["tempPassword"];
-    time: User["tempPasswordTime"];
-    newPassword: User["password"];
-  };
+	forgotPasswordInput: {
+		email: User["email"];
+		otp: User["tempPassword"];
+		time: User["tempPasswordTime"];
+		newPassword: User["password"];
+	};
 }
 
 export const AuthResolvers = {
-  Query: {
-    getUserInfo: async (_: any, __: any, ctx: CustomContext): Promise<User> => {
-      try {
-        return ctx?.User;
-      } catch (err) {
-        const error = await generateErrorObject(
-          Errors.SESSION_EXPIRED,
-          String(err),
-          ctx
-        );
-        throw error;
-      }
-    },
-    getAllUsers: async (
-      _: any,
-      __: any,
-      ctx: CustomContext
-    ): Promise<User[] | undefined> => {
-      try {
-        const Users = await getAllUsers();
-        console.log(Users);
-        return Users;
-      } catch (err) {
-        const error = await generateErrorObject(
-          Errors.SESSION_EXPIRED,
-          String(err),
-          ctx
-        );
-        throw error;
-      }
-    },
-  },
-  Mutation: {
-    newUser: async (
-      _: any,
-      { userInput }: UserInput,
-      ctx: CustomContext
-    ): Promise<User> => {
-      const { email } = userInput;
+	Query: {
+		getUserInfo: async (_: any, __: any, ctx: CustomContext): Promise<User> => {
+			try {
+				return ctx?.User;
+			} catch (err) {
+				const error = await generateErrorObject(
+					Errors.SESSION_EXPIRED,
+					String(err),
+					ctx
+				);
+				throw error;
+			}
+		},
+		getAllUsers: async (
+			_: any,
+			__: any,
+			ctx: CustomContext
+		): Promise<User[] | undefined> => {
+			try {
+				const Users = await getAllUsers();
+				console.log(Users);
+				return Users;
+			} catch (err) {
+				const error = await generateErrorObject(
+					Errors.SESSION_EXPIRED,
+					String(err),
+					ctx
+				);
+				throw error;
+			}
+		},
+	},
+	Mutation: {
+		newUser: async (
+			_: any,
+			{ userInput }: UserInput,
+			ctx: CustomContext
+		): Promise<User> => {
+			const { email } = userInput;
 
-      if (!/@jardinbinario.com\s*$/.test(email)) {
-        throw await generateErrorObject(
-          Errors.NOT_VALID_DOMAIN,
-          `${email} does not have a valid domain.`,
-          ctx
-        );
-      }
+			if (!/@jardinbinario.com\s*$/.test(email)) {
+				throw await generateErrorObject(
+					Errors.NOT_VALID_DOMAIN,
+					`${email} does not have a valid domain.`,
+					ctx
+				);
+			}
 
-      const User = await findUserByEmail(email);
+			const User = await findUserByEmail(email);
 
-      if (User) {
-        throw await generateErrorObject(
-          Errors.DUPLICATED_RECORD,
-          `${email} it's already in use.`,
-          ctx
-        );
-      }
+			if (User) {
+				throw await generateErrorObject(
+					Errors.DUPLICATED_RECORD,
+					`${email} it's already in use.`,
+					ctx
+				);
+			}
 
-      try {
-        const newUser = await new UserModel(userInput).save();
-        return newUser;
-      } catch (err) {
-        throw await generateErrorObject(
-          Errors.INTERNAL_SERVER_ERROR,
-          String(err),
-          ctx
-        );
-      }
-    },
-    toggleUserActive: async (
-      _: any,
-      { email }: Record<string, string>,
-      ctx: CustomContext
-    ): Promise<User> => {
+			try {
+				const newUser = await new UserModel(userInput).save();
+				return newUser;
+			} catch (err) {
+				throw await generateErrorObject(
+					Errors.INTERNAL_SERVER_ERROR,
+					String(err),
+					ctx
+				);
+			}
+		},
+		toggleUserActive: async (
+			_: any,
+			{ email }: Record<string, string>,
+			ctx: CustomContext
+		): Promise<User> => {
 
-      if (!/@jardinbinario.com\s*$/.test(email)) {
-        throw await generateErrorObject(
-          Errors.NOT_VALID_DOMAIN,
-          `${email} does not have a valid domain.`,
-          ctx
-        );
-      }
+			if (!/@jardinbinario.com\s*$/.test(email)) {
+				throw await generateErrorObject(
+					Errors.NOT_VALID_DOMAIN,
+					`${email} does not have a valid domain.`,
+					ctx
+				);
+			}
 
-      try {
-        const deactivadedUser = await toggleActive(email);
+			try {
+				const deactivadedUser = await toggleActive(email);
 
-        if (!deactivadedUser) {
-          throw await generateErrorObject(
-            Errors.NOT_FOUND,
-            `${email} is not a valid user.`,
-            ctx
-          );
-        }
+				if (!deactivadedUser) {
+					throw await generateErrorObject(
+						Errors.NOT_FOUND,
+						`${email} is not a valid user.`,
+						ctx
+					);
+				}
 
-        return deactivadedUser;
-      } catch (err) {
-        throw await generateErrorObject(
-          Errors.INTERNAL_SERVER_ERROR,
-          String(err),
-          ctx
-        );
-      }
-    },
-    authenticate: async (
-      _: any,
-      { authInput }: AuthInput,
-      ctx: CustomContext
-    ): Promise<Token> => {
-      const { email, password } = authInput;
-      const User = await findUserByEmail(email);
+				return deactivadedUser;
+			} catch (err) {
+				throw await generateErrorObject(
+					Errors.INTERNAL_SERVER_ERROR,
+					String(err),
+					ctx
+				);
+			}
+		},
+		authenticate: async (
+			_: any,
+			{ authInput }: AuthInput,
+			ctx: CustomContext
+		): Promise<Token> => {
+			const { email, password } = authInput;
+			const User = await findUserByEmail(email);
 
-      if (!User) {
-        throw await generateErrorObject(
-          Errors.UNKOWN_USER,
-          `A user for ${email} was not found.`,
-          ctx
-        );
-      }
+			if (!User) {
+				throw await generateErrorObject(
+					Errors.UNKOWN_USER,
+					`A user for ${email} was not found.`,
+					ctx
+				);
+			}
 
-      if (!User.checkPassword(password)) {
-        throw await generateErrorObject(
-          Errors.WRONG_PASSWORD,
-          "Incorrect password.",
-          ctx
-        );
-      }
+			if (!User.checkPassword(password)) {
+				throw await generateErrorObject(
+					Errors.WRONG_PASSWORD,
+					"Incorrect password.",
+					ctx
+				);
+			}
 
-      try {
-        const token = generateJWT(User as User);
-        return { token };
-      } catch (err) {
-        throw await generateErrorObject(
-          Errors.INTERNAL_SERVER_ERROR,
-          String(err),
-          ctx
-        );
-      }
-    },
-    initForgotPassword: async (
-      _: any,
-      { email }: Record<string, string>,
-      ctx: CustomContext
-    ): Promise<string> => {
-      const User = await findUserByEmail(email);
+			try {
+				const token = generateJWT(User as User);
+				return { token };
+			} catch (err) {
+				throw await generateErrorObject(
+					Errors.INTERNAL_SERVER_ERROR,
+					String(err),
+					ctx
+				);
+			}
+		},
+		initForgotPassword: async (
+			_: any,
+			{ email }: Record<string, string>,
+			ctx: CustomContext
+		): Promise<string> => {
+			const User = await findUserByEmail(email);
 
-      if (!User) {
-        throw await generateErrorObject(
-          Errors.UNKOWN_USER,
-          `A user for ${email} was not found.`,
-          ctx
-        );
-      }
+			if (!User) {
+				throw await generateErrorObject(
+					Errors.UNKOWN_USER,
+					`A user for ${email} was not found.`,
+					ctx
+				);
+			}
 
-      try {
-        const random = v4().split("-")[0];
-        const notified = await notifyUserAboutForgotPassword(User, random);
+			try {
+				const random = v4().split("-")[0];
+				const notified = await notifyUserAboutForgotPassword(User, random);
 
-        if (!notified) {
-          throw await generateErrorObject(
-            Errors.SMTP_ERROR,
-            `There was an error reaching ${User.email}`,
-            ctx
-          );
-        }
+				if (!notified) {
+					throw await generateErrorObject(
+						Errors.SMTP_ERROR,
+						`There was an error reaching ${User.email}`,
+						ctx
+					);
+				}
 
-        User.tempPassword = random;
-        await User.save();
+				User.tempPassword = random;
+				await User.save();
 
-        return `We've sent you a token at ${User.email}`;
-      } catch (err) {
-        throw await generateErrorObject(
-          Errors.INTERNAL_SERVER_ERROR,
-          String(err),
-          ctx
-        );
-      }
-    },
-    finishForgotPassword: async (
-      _: any,
-      { forgotPasswordInput }: ForgotPasswordInput,
-      ctx: CustomContext
-    ): Promise<User> => {
-      const { email, otp, newPassword, time } = forgotPasswordInput;
-      const User = await findUserByEmail(email);
+				return `We've sent you a token at ${User.email}`;
+			} catch (err) {
+				throw await generateErrorObject(
+					Errors.INTERNAL_SERVER_ERROR,
+					String(err),
+					ctx
+				);
+			}
+		},
+		finishForgotPassword: async (
+			_: any,
+			{ forgotPasswordInput }: ForgotPasswordInput,
+			ctx: CustomContext
+		): Promise<User> => {
+			const { email, otp, newPassword, time } = forgotPasswordInput;
+			const User = await findUserByEmail(email);
 
-      if (!User) {
-        throw await generateErrorObject(
-          Errors.UNKOWN_USER,
-          `A user for ${email} was not found.`,
-          ctx
-        );
-      }
+			if (!User) {
+				throw await generateErrorObject(
+					Errors.UNKOWN_USER,
+					`A user for ${email} was not found.`,
+					ctx
+				);
+			}
 
-      const diff = +new Date() - +new Date(String(time));
-      const seconds = Math.floor(diff / 1000);
+			const diff = +new Date() - +new Date(String(time));
+			const seconds = Math.floor(diff / 1000);
 
-      if (!(seconds < 3600)) {
-        throw await generateErrorObject(
-          Errors.TIMEOUT,
-          "The code used has expired.",
-          ctx
-        );
-      }
+			if (!(seconds < 3600)) {
+				throw await generateErrorObject(
+					Errors.TIMEOUT,
+					"The code used has expired.",
+					ctx
+				);
+			}
 
-      if (!User.checkPassword(otp, true)) {
-        throw await generateErrorObject(
-          Errors.UNAUTHORIZED,
-          "The code provided is invalid.",
-          ctx
-        );
-      }
+			if (!User.checkPassword(otp, true)) {
+				throw await generateErrorObject(
+					Errors.UNAUTHORIZED,
+					"The code provided is invalid.",
+					ctx
+				);
+			}
 
-      User.password = newPassword;
-      User.tempPassword = undefined;
-      User.tempPasswordTime = undefined;
+			User.password = newPassword;
+			User.tempPassword = undefined;
+			User.tempPasswordTime = undefined;
 
-      const updatedUser = await User.save();
-      return updatedUser;
-    },
-  },
+			const updatedUser = await User.save();
+			return updatedUser;
+		},
+	},
 };
--- src/helpers/getCustomContext.ts
+++ src/helpers/getCustomContext.ts
@@ -5,108 +5,108 @@ import { verifyJWT } from "./authFunctions";
 import { Errors, generateErrorObject } from "./Logger";
 
 interface ApolloRequest extends IncomingMessage {
-  body?: any;
+	body?: any;
 }
 
 const queriesThatDontRequireAuthentication = [
-  "authenticate",
-  "introspectionquery",
-  "initforgotpassword",
-  "finishforgotpassword",
-  "getspecificblogentry",
-  "newticket",
-  "newnewsletterentry",
-  "getrecententries",
-  "getmostviewedentries",
-  "getallentriesids",
-  "updateblogmetrics",
-  "newuserdetailsentry",
+	"authenticate",
+	"introspectionquery",
+	"initforgotpassword",
+	"finishforgotpassword",
+	"getspecificblogentry",
+	"newticket",
+	"newnewsletterentry",
+	"getrecententries",
+	"getmostviewedentries",
+	"getallentriesids",
+	"updateblogmetrics",
+	"newuserdetailsentry",
 	"getmetric",
 ];
 
 const getQueryName = (body: any): string => {
-  let { query: unparsedQuery } = body;
-  unparsedQuery = unparsedQuery.trim();
-  const type = unparsedQuery.split(" ")[0];
+	let { query: unparsedQuery } = body;
+	unparsedQuery = unparsedQuery.trim();
+	const type = unparsedQuery.split(" ")[0];
 
-  switch (type) {
-    case "mutation": {
-      const queryWithoutType = unparsedQuery.substring(9);
-      const toReturn = queryWithoutType.substring(
-        0,
-        queryWithoutType.indexOf("(")
-      );
-      return toReturn.trim();
-    }
-    case "query": {
-      const queryWithoutType = unparsedQuery.substring(6).split("\n")[0];
-      let toReturn;
-      const parenthesis = queryWithoutType.indexOf("(");
-      const curlyBrace = queryWithoutType.indexOf("{");
+	switch (type) {
+		case "mutation": {
+			const queryWithoutType = unparsedQuery.substring(9);
+			const toReturn = queryWithoutType.substring(
+				0,
+				queryWithoutType.indexOf("(")
+			);
+			return toReturn.trim();
+		}
+		case "query": {
+			const queryWithoutType = unparsedQuery.substring(6).split("\n")[0];
+			let toReturn;
+			const parenthesis = queryWithoutType.indexOf("(");
+			const curlyBrace = queryWithoutType.indexOf("{");
 
-      if (parenthesis > 0 && parenthesis < curlyBrace) {
-        toReturn = queryWithoutType.substring(0, queryWithoutType.indexOf("("));
-      } else {
-        toReturn = queryWithoutType.substring(0, queryWithoutType.indexOf("{"));
-      }
+			if (parenthesis > 0 && parenthesis < curlyBrace) {
+				toReturn = queryWithoutType.substring(0, queryWithoutType.indexOf("("));
+			} else {
+				toReturn = queryWithoutType.substring(0, queryWithoutType.indexOf("{"));
+			}
 
-      return toReturn.trim();
-    }
-    default: {
-      return "";
-    }
-  }
+			return toReturn.trim();
+		}
+		default: {
+			return "";
+		}
+	}
 };
 
 export const getCustomContext = async (
-  req: ApolloRequest,
+	req: ApolloRequest,
 ): Promise<CustomContext | TaggedContext> => {
-  const token = req.headers?.["authorization"] || "";
-  const tokenWithoutBearer = token.replace("Bearer", "").trim();
-  const query = req.body?.operationName || getQueryName(req.body);
-  const taggedContext = initContextTagging(query);
+	const token = req.headers?.["authorization"] || "";
+	const tokenWithoutBearer = token.replace("Bearer", "").trim();
+	const query = req.body?.operationName || getQueryName(req.body);
+	const taggedContext = initContextTagging(query);
 
-  if (queriesThatDontRequireAuthentication.includes(query.toLowerCase()))
-    return taggedContext;
+	if (queriesThatDontRequireAuthentication.includes(query.toLowerCase()))
+		return taggedContext;
 
-  if (!token || tokenWithoutBearer === "")
-    throw new Error("A verification token is required.");
+	if (!token || tokenWithoutBearer === "")
+		throw new Error("A verification token is required.");
 
-  try {
-    const User = verifyJWT(tokenWithoutBearer);
+	try {
+		const User = verifyJWT(tokenWithoutBearer);
 
-    if (!User) {
-      throw await generateErrorObject(
-        Errors.UNKOWN_USER,
-        "Not a valid user",
-        taggedContext
-      );
-    }
+		if (!User) {
+			throw await generateErrorObject(
+				Errors.UNKOWN_USER,
+				"Not a valid user",
+				taggedContext
+			);
+		}
 
-    return {
-      User,
-      ...taggedContext,
-    };
-  } catch (err) {
-    let error = err as Error;
+		return {
+			User,
+			...taggedContext,
+		};
+	} catch (err) {
+		let error = err as Error;
 
-    if (error.message === "jwt expired") {
-      error = "Session expired." as any;
-    }
+		if (error.message === "jwt expired") {
+			error = "Session expired." as any;
+		}
 
-    throw await generateErrorObject(
-      Errors.INTERNAL_SERVER_ERROR,
-      String(error),
-      taggedContext
-    );
-  }
+		throw await generateErrorObject(
+			Errors.INTERNAL_SERVER_ERROR,
+			String(error),
+			taggedContext
+		);
+	}
 };
 
 const initContextTagging = (query: string): TaggedContext => {
-  const requestId = v4();
+	const requestId = v4();
 
-  return {
-    requestId,
-    query,
-  };
+	return {
+		requestId,
+		query,
+	};
 };
